e865b3ce3e41836d9de059153f42c034d37855fa
Ville M. Vainio <ville.vainio@basware.com>
2015-04-20 18:24:39 +0300

    add setup.py

---
+++ grepo/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='grepo',
+      version='0.1.1',
+      description='Grep and peco tool',
+      author='Ville Vainio',
+      author_email='vivainio@gmail.com',
+      url='https://github.com/vivainio/grepo',
+      packages=['grepo'],
+      entry_points = {
+        'console_scripts': [
+            'grepo = grepo.grepo:main',
+        ]
+      }
+     )

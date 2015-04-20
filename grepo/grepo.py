381646b50793a8a74064fa61e184c65c98cc3cea
Ville M. Vainio <ville.vainio@basware.com>
2015-04-20 21:02:04 +0300

    use temp file (for speed)

--- grepo/grepo.py
+++ grepo/grepo.py
@@ -1,7 +1,8 @@
 import pickleshare
 import args
-import os
+import os, glob
 from subprocess import *
+import tempfile
 
 db = pickleshare.PickleShareDB("~/.grepo")
 
@@ -11,13 +12,43 @@ def here_c(args):
 	db['root'] = os.getcwd()
 
 def runpeco(input):
-	p = Popen('peco', stdin = PIPE, stdout = PIPE)
-	fin, fout = p.stdin, p.stdout
-	fin.write(input)
-	fin.close()
-	out = fout.read()
+	tf = tempfile.NamedTemporaryFile(delete=False)
+	tf.write(input)
+	tf.close()
+	p = Popen(['peco', '-b', '5000000', tf.name], stdout = PIPE)
+	out = p.stdout.read()
+	os.unlink(tf.name)
+	#fin, fout = p.stdin, p.stdout
+	#fin.write(input)
+	#fin.close()
+	
 	return out
 
+def parents(pth):
+	cur = pth
+	while 1:
+		c2 = os.path.dirname(cur)
+		if c2 == cur:
+			return
+		cur = c2
+		yield cur
+
+def search_str(s, files):
+	res = []
+	for f in files:
+		if s in open(f).read():
+			res.append(f)
+
+	return res
+
+def more_info(fname):
+	fname = os.path.abspath(fname)
+	print fname
+	bname = os.path.basename(fname)
+	for p in parents(fname):
+		look = glob.glob(p + '/*.csproj') + glob.glob(p + '/*.sln')
+		found = search_str(bname, look)
+		print p, look, found
 
 def peco_and_edit(input):
 	lines = runpeco(input).splitlines()
@@ -25,8 +56,7 @@ def peco_and_edit(input):
 		fname, line, text = l.split(':', 2)
 		print "pick:", fname
 		call(['subl',fname + ':' + line])
-
-
+		more_info(fname)
 
 
 def grep_c(args):
@@ -51,6 +81,9 @@ def main():
 	s.arg("pattern", type=str)
 	args.sub('p', pick_c, help = 'Use peco to quick pick one of the earlier choices')
 	args.parse()
+
+def test():
+	pick_c(None)
 
 if __name__ == "__main__":
 	main()
